3fccf4a199aedab926e343b520d899bbd32a421e
John Ashley Torio <johnashley132002@gmail.com>
2025-11-05 14:37:38 +0800

    Page routing

---
+++ app/routes/home/index.tsx
@@ -0,0 +1,14 @@
+import type { Route } from "./+types/index";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Portfolio Dev | Welcome" },
+    { name: "description", content: "Custom portfolio dev development" },
+  ];
+}
+
+export default function Home() {
+  return <section>
+  My App 
+  </section>;
+}
